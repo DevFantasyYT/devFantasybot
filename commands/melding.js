ba5df8cfedf0dd1d2d1d833f2665aacbbbe69bf7
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-06 12:18:42 +0100

    init

---
+++ commands/melding.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, arguments) => {
+    
+    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("Sorry u kan dit niet doen!");
+    
+    var idee = arguments.join(' ');
+
+    if (!idee) return message.channel.send("**Geef een mededeling mee!**");
+
+    var ideeEmbed = new discord.RichEmbed()
+        .setTitle("**Nieuw mededeling!**")
+        .setColor("#35fc03")
+        .addField("Melding:", idee)
+        .addFooter("Ingezonden door:", message.author);
+
+    var mmeldingChannel = message.guild.channels.find("name", "✉〉mededelingen");
+    if (!meldingChannel) return message.channel.send("Kanaal niet gevonden!");
+
+    meldingChannel.send(meldingEmbed)
+
+}
+
+module.exports.help = {
+    name: "melding"
+}
