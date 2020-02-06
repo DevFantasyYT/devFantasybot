58e7b2c8a73be98c4ed8b4a81f4953d7edda83f6
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-06 12:40:46 +0100

    Update helpadmin.js

--- commands/helpadmin.js
+++ commands/helpadmin.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, arguments) =>{
 
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry u kan dit niet doen!");
 
-        var text = "**__Admin Commands__** \n\n `!kick` - Kick een user. \n `!ban` - Ban een speler. \n `!tempmute` - Mute iemand voor een bepaalde tijd | *!tempmute {gebruiker} {tijd}* \n `!clear` - Verwijder aantal berichten. | **Nooit hoger opgeven dan 100!!** \n `!warn` - Waarschuw een gebruiker. \n `!close` - Sluit een ticket!";
+        var text = "**__Admin Commands__** \n\n `!kick` - Kick een user. \n `!ban` - Ban een speler. \n `!tempmute` - Mute iemand voor een bepaalde tijd | *!tempmute {gebruiker} {tijd}* \n `!clear` - Verwijder aantal berichten. | **Nooit hoger opgeven dan 100!!** \n `!warn` - Waarschuw een gebruiker. \n `!close` - Sluit een ticket! \n `!mededeling` - Plaats een mededeling.";
 
         message.author.send(text);
 
