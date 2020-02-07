ba7b95a746bd12dd93beb46694b400a12628c3f7
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-07 10:02:50 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -6,17 +6,17 @@ module.exports.run = async (bot, message, args) => {
     var user = message.mentions.users.first() || message.author;
 
     var embedCreate = new discord.RichEmbed()
-        .setAuthor(user.tag, user.avatarURL)
-        .setColor("RED")
-        .setThumbmail(user.avatarURL)
-        .setTimestamp()
-        .setFooter("Gebruiker informatie!")
-        .addField("Naam:", user.username)
-        .addField("Status:", user.presence.status)
-        .addField("Gejoint op", moment(message.guild.members.get(user.id).joinedAt).format("MMM Do YY"))
-        .addField("Account gemaakt op:", moment(message.guild.members.get(user.id).createAt).format("MMM Do YY"));
+    .setAuthor(user.tag, user.avatarURL)
+    .setColor("RED")
+    .setThumbmail(user.avatarURL)
+    .setTimestamp()
+    .setFooter("Gebruiker informatie!")
+    .addField("Naam:", user.username)
+    .addField("Status:", user.presence.status)
+    .addField("Gejoint op", moment(message.guild.members.get(user.id).joinedAt).format("MMM Do YY"))
+    .addField("Account gemaakt op:", moment(message.guild.members.get(user.id).createAt).format("MMM Do YY"));
 
-    message.channel.send(embedCreate); 
+    message.channel.send(embedCreate);
 }
 
 module.exports.help = {
