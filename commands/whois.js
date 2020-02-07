8fee9fde08bf67595a016ee2704fd0b4923e9972
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-07 10:14:01 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -3,13 +3,20 @@ const moment = require('moment');
 
 module.exports.run = async (bot, message, args) => {
 
-    var user = message.author;
+    var user = message.guild.member(message.mentions.users.first() || message.author);
 
-    var createEmbed = new Discord.RichEmbed()
-    .addField("Testje", user);
+    var embedCreate = new discord.RichEmbed()
+        .setAuthor(user.tag, user.avatarURL)
+        .setColor("RED")
+        .setThumbmail(user.avatarURL)
+        .setTimestamp()
+        .setFooter("Gebruiker informatie!")
+        .addField("Naam:", user.username)
+        .addField("Status:", user.presence.status)
+        .addField("Gejoint op", moment(message.guild.members.get(user.id).joinedAt).format("MMM Do YY"))
+        .addField("Account gemaakt op:", moment(message.guild.members.get(user.id).createAt).format("MMM Do YY"));
 
-    message.channel.send(createEmbed)
-
+    message.channel.send(embedCreate);
 }
 
 module.exports.help = {
