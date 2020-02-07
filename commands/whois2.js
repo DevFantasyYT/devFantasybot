94216e992bedfbee0c009664c245f5ed26c5f9e7
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-07 10:31:47 +0100

    Update whois2.js

--- commands/whois2.js
+++ commands/whois2.js
@@ -2,10 +2,12 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, arguments) =>{
 
+    var icon = message.guild.iconURL;
+
     const exampleEmbed = new discord.RichEmbed()
 	.setColor('#0099ff')
 	.setTitle('Profiel van:', user.username)
-	.setThumbnail(user.avatarURL)
+	.setThumbnail(icon)
 	.addField('Regular field title', 'Some value here')
 	.addField('Inline field title', 'Some value here', true)
 	.addField('Inline field title', 'Some value here', true)
