1762b0b2fc7b88cf0095da805d85469464f3dc21
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-10 12:59:11 +0100

    Update whois2.js

--- commands/whois2.js
+++ commands/whois2.js
@@ -3,15 +3,15 @@ const moment = require("moment");
 
 module.exports.run = async (bot, message, arguments) =>{
 
-    var icon = message.guild.iconURL;
+    var icon = message.author.avatarURL;
 
     const exampleEmbed = new discord.RichEmbed()
 	.setColor('#0099ff')
 	.setTitle(`Profiel van: ${message.author.username}`,)
 	.setThumbnail(icon)
 	.addField('ID:', message.member.id)
-	.addField('Gejoint op:',  moment(message.guild.members.get.joinedAt).format("Do MMM YY"))
-	.addField('Account:', moment(message.guild.members.get.createAt).format("Do MMM YY"))
+	.addField('Gejoint op:',  moment(message.guild.members.joinedAt).format("Do MMM YY"))
+	.addField('Account:', moment(message.guild.members.createAt).format("Do MMM YY"))
 	.setTimestamp()
     .setFooter('Profiel info!');
     
