c902dd535ad01f446fc209830fbd63b298d7496e
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-10 13:08:12 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -3,17 +3,17 @@ const moment = require("moment");
 
 module.exports.run = async (bot, message, arguments) =>{
 
-	var icon = message.mentions.users.first.avatarURL() || message.author.avatarURL;
+	var icon = message.mentions.users.first() || message.author.avatarURL;
 	
 	var user = message.mentions.users.first() || message.author;
 
     const exampleEmbed = new discord.RichEmbed()
 	.setColor('#9e5811')
-	.setTitle(`Profiel van: ${user.username}`,)
+	.setTitle(`Profiel van: ${message.user.username}`,)
 	.setThumbnail(icon)
-	.addField('ID:', user.id)
-	.addField('Gejoint op:',  moment(user.guild.joinedAt).format("Do MMM YY"))
-	.addField('Account:', moment(user.guild.createAt).format("Do MMM YY"))
+	.addField('ID:', message.user.id)
+	.addField('Gejoint op:',  moment(message.user.joinedAt).format("Do MMM YY"))
+	.addField('Account:', moment(message.user.guild.createAt).format("Do MMM YY"))
 	.setTimestamp()
     .setFooter('Profiel info!');
     
