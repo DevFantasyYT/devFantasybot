b35c4e35774fd1362c4b85b9bd049f2ad2b5f845
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-02-12 15:54:46 +0100

    q

--- commands/whois.js
+++ commands/whois.js
@@ -1,43 +1,26 @@
-const { RichEmbed } = require("discord.js");
-const { stripIndents } = require("common-tags");
-const { getMember, formatDate } = require("../../functions.js");
+const discord = require("discord.js");
+const moment = require("moment");
 
-module.exports = {
-    name: "whois",
-    aliases: ["who", "user", "info"],
-    description: "Returns user information",
-    usage: "[username | id | mention]",
-    run: (client, message, args) => {
-        const member = getMember(message, args.join(" "));
+module.exports.run = async (bot, message, arguments) =>{
 
-        // Member variables
-        const joined = formatDate(member.joinedAt);
-        const roles = member.roles
-            .filter(r => r.id !== message.guild.id)
-            .map(r => r).join(", ") || 'none';
+	var icon = message.mentions.users.first() || message.author.avatarURL;
+	
+	var user = message.mentions.users.first() || message.author;
 
-        // User variables
-        const created = formatDate(member.user.createdAt);
+    const exampleEmbed = new discord.RichEmbed()
+	.setColor('#9e5811')
+	.setTitle(`Profiel van: ${user.username}`,)
+	.setThumbnail(icon)
+	.addField('ID:', user.id)
+	.addField('Gejoint op:',  moment(user.joinedAt).format("Do MMM YY"))
+	.addField('Account:', moment(user.createAt).format("Do MMM YY"))
+	.setTimestamp()
+    .setFooter('Profiel info!');
+    
+    message.channel.send(exampleEmbed);
 
-        const embed = new RichEmbed()
-            .setFooter(member.displayName, member.user.displayAvatarURL)
-            .setThumbnail(member.user.displayAvatarURL)
-            .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+}
 
-            .addField('Member information:', stripIndents`**- Display name:** ${member.displayName}
-            **- Joined at:** ${joined}
-            **- Roles:** ${roles}`, true)
-
-            .addField('User information:', stripIndents`**- ID:** ${member.user.id}
-            **- Username**: ${member.user.username}
-            **- Tag**: ${member.user.tag}
-            **- Created at**: ${created}`, true)
-            
-            .setTimestamp()
-
-        if (member.user.presence.game) 
-            embed.addField('Currently playing', stripIndents`** Name:** ${member.user.presence.game.name}`);
-
-        message.channel.send(embed);
-    }
+module.exports.help = {
+    name: "whois"
 }
